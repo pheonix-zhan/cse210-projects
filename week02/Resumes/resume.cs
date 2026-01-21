43ea8bccdf3b1eba193a89eace252257d87c414d
sandysunshine <snankya@byupathway.edu>
2026-01-21 23:12:07 +0300

    job

---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,19 @@
+public class Resume
+{
+    public string _name;
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        // Notice the use of the custom data type "Job" in this loop
+        foreach ( Job j in _jobs)
+        {
+            // This calls the Display method on each job
+            j.DisplayJob();
+        }
+    }
+
+}
